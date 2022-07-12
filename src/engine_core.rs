fdc9ce6f73547598246907ba2c751147f14195bf
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-07-12 19:04:18 -0400

    Added the basic structure of the engine core.

---
+++ src/engine_core.rs
@@ -0,0 +1,11 @@
+pub fn init() {
+    println!("[INFO]: Initializing the Nengine!");
+}
+
+pub fn update() {
+    println!("[INFO]: Updating the Nengine!");
+}
+
+pub fn stop() {
+    println!("[INFO]: Stopping the Nengine!");
+}
