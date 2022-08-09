3bc8bbaba7badca8d9a39f7cd36ee3663057830a
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-09 15:09:10 -0400

    Rewrote the Engine core and setted up a workspace.
    Co-authored-by: carpetmaker3162 <carpetmaker3162@users.noreply.github.com>

---
+++ nengine/src/core.rs
@@ -0,0 +1,23 @@
+pub struct Nengine {}
+
+impl Nengine {
+    pub fn new() -> Nengine {
+        println!("[INFO]: Initialized the Nengine.");
+
+        return Nengine {};
+    }
+    
+    pub fn is_running() -> bool {
+        return true;
+    }
+    
+    pub fn update(&mut self) {
+        println!("[INFO]: Updating the Nengine.");
+    }
+}
+
+impl Drop for Nengine {
+    fn drop(&mut self) {
+        println!("[INFO]: Shutting down the Nengine.");
+    }
+}
--- src/bin/game_launcher.rs
+++
@@ -1,9 +0,0 @@
-use nengine::engine_core;
-
-fn main() {
-    engine_core::init();
-    
-    while engine_core::update() {}
-    
-    engine_core::stop();
-}
