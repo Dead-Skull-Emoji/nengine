2ee7c15f37d70608f4d9ac1ceea318a0112406f7
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-11 12:57:05 -0400

    Tested instance extension enumeration.

--- nengine_sandbox/src/main.rs
+++ nengine_sandbox/src/main.rs
@@ -1,7 +1,7 @@
-use nengine_vulkan::backend::{
-    Instance
-};
+use nengine_vulkan::backend::Instance;
 
 fn main() {
     let _instance = Instance::new("Nengine Sandbox", 0, true).unwrap();
+    
+    Instance::enumerate_instance_extension_names().iter().for_each(|extension| println!("[INFO]: Found Vulkan extension {}", extension));
 }
