948b67ce8ce1db70f47d82e226c1de9e924ab5f5
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-11 13:15:41 -0400

    Tested the two functionalities added.

--- nengine_sandbox/src/main.rs
+++ nengine_sandbox/src/main.rs
@@ -1,7 +1,17 @@
 use nengine_vulkan::backend::Instance;
 
 fn main() {
-    let _instance = Instance::new("Nengine Sandbox", 0, true).unwrap();
-    
-    Instance::enumerate_instance_extension_names().iter().for_each(|extension| println!("[INFO]: Found Vulkan extension {}", extension));
+    Instance::enumerate_instance_extension_names()
+        .iter()
+        .for_each(|extension| println!("[INFO]: Found Vulkan extension {}", extension));
+    let instance = Instance::new("Nengine Sandbox", 0, true).unwrap();
+    instance
+        .enumerate_physical_devices()
+        .iter()
+        .for_each(|device| {
+            println!(
+                "[INFO]: Found Vulkan physical device {}",
+                device.get_properties().device_name
+            )
+        });
 }
