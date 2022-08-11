20222fcd4d69ee71693d91b5046b25267a7d3828
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-11 09:30:42 -0400

    Added Vulkan FFI bindings.

---
+++ nengine_vulkan/build.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    
+    if target_os == "linux" {
+        let outpath = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+        
+        bindgen::builder()
+            .header("src/raw.h")
+            .prepend_enum_name(false)
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Failed to generate FFI bindings")
+            .write_to_file(outpath.join("raw.rs"))
+            .expect("Failed to write bindings to a file");
+    } else if target_os == "windows" {
+        panic!("Windows is not supported yet.");
+    }
+}
