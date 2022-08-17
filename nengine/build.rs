5a8f82a3b0f54c4261e31dfba6ad623af0cca765
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-17 11:47:30 -0400

    Setted up Rust bindings for XCB.

---
+++ nengine/build.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let out_path = std::path::PathBuf::from(out_dir);
+
+    if target_os == "linux" {
+        bindgen::builder()
+            .header("src/ffi/xcb.h")
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Failed to generate bindings for XCB")
+            .write_to_file(out_path.join("xcb_bindings.rs"))
+            .expect("Failed to write bindings to a file");
+    } else {
+        panic!("Only Linux is supported for now.")
+    }
+}
--- nengine/src/lib.rs
+++ nengine/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod core;
+pub mod ffi;
 
 #[cfg(test)]
 mod tests {
