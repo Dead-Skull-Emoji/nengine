49bdf108714c98cbff4ed87c3623165d89edfebe
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-08-18 10:46:43 -0400

    Added support for Windows.

--- nengine_vulkan/build.rs
+++ nengine_vulkan/build.rs
@@ -15,6 +15,22 @@ fn main() {
             .write_to_file(outpath.join("raw.rs"))
             .expect("Failed to write bindings to a file");
     } else if target_os == "windows" {
-        panic!("Windows is not supported yet.");
+        let outpath = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+        let vulkan_location = std::env::var("VULKAN_SDK").expect("Unable to find the Vulkan SDK!");
+        
+        println!("cargo:rustc-link-search={}", vulkan_location.clone() + "\\Lib");
+        println!("cargo:rustc-link-lib=vulkan-1");
+        
+        bindgen::builder()
+            .clang_arg(format!("-I{}", vulkan_location + "\\Include"))
+            .header("src/backend/raw.h")
+            .prepend_enum_name(false)
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Failed to generate FFI bindings")
+            .write_to_file(outpath.join("raw.rs"))
+            .expect("Failed to write bindings to a file");
+    } else {
+        panic!("Unsupported operating system!");
     }
 }
