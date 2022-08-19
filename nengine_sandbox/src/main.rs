3b2861a68c46d12bf606c026430a8f6bd4f1dfdd
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-19 16:17:49 -0400

    Tested event handling.

--- nengine_sandbox/src/main.rs
+++ nengine_sandbox/src/main.rs
@@ -1,11 +1,24 @@
-use nengine::platform::Window;
 use nengine::platform::CrossPlatformWindow;
+use nengine::platform::Window;
+use nengine::Event;
+use nengine::MouseButton;
 
 fn main() {
-    let mut window = Window::new(800, 600, "Nengine Sandbox", true);
-    
+    let mut window = Window::new(800, 600, "Nengine Sandbox", false);
+    window.set_event_callback(|event| {
+        if let Event::MouseButton { button, is_press } = event {
+            if is_press {
+                if let MouseButton::Left = button {
+                    println!("Left mouse button!");
+                } else if let MouseButton::Right = button {
+                    println!("Right mouse button!");
+                }
+            }
+        }
+    });
+
     window.show();
-    
+
     while window.is_open() {
         window.poll_events();
     }
