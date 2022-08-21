b340acdd7b446049ef4b7420daa74236d1a26d13
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-08-21 12:23:16 -0400

    Tested the button release event handler.

--- nengine_sandbox/src/main.rs
+++ nengine_sandbox/src/main.rs
@@ -1,12 +1,21 @@
 use nengine::platform::CrossPlatformWindow;
 use nengine::platform::Window;
 use nengine::Event;
+use nengine::MouseButton;
 
 fn main() {
     let mut window = Window::new(800, 600, "Nengine Sandbox", false);
     window.set_event_callback(|event| {
         if let Event::MouseScroll { x, y } = event {
             println!("[INFO]: Scroll event: {} {}", x, y);
+        } else if let Event::MouseButton { button, is_press } = event {
+            if let MouseButton::Right = button {
+                if !is_press {
+                    println!("[INFO]: Right click button released!");
+                } else {
+                    println!("[INFO]: Right click button pressed!");
+                }
+            }
         }
     });
 
